799cd511e3453c04eb55929f23b5fd14ea5b9a85
Phương Trịnh <thaophuong.trinh.9@gmail.com>
2020-12-14 19:54:52 +0700

    Create base for project with empty activity

---
+++ app/src/main/java/vn/edu/usth/minigh/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.usth.minigh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
