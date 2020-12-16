36e3b7e1c8851fd65f64c6473cf9163245010e14
Ngô Xuân Minh <48543141+9a24f0@users.noreply.github.com>
2020-12-16 14:17:00 +0700

    Add login activity
    For real this time

---
+++ app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
@@ -0,0 +1,12 @@
+package vn.edu.usth.minigh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    public AuthActivity() {
+        super(R.layout.activity_auth);
+    }
+}
