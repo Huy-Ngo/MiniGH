1ff7ed23994b2f4b036b1e5229ec9b74041aba3a
Ngô Xuân Minh <48543141+9a24f0@users.noreply.github.com>
2020-12-18 15:04:13 +0700

    Make session management for authentication
    * Remove login text
    * Add landscape design
    * Make session management for authentication

--- app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
+++ app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
@@ -2,11 +2,49 @@ package vn.edu.usth.minigh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AuthActivity extends AppCompatActivity {
 
     public AuthActivity() {
         super(R.layout.activity_auth);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkSession();
+    }
+
+    private void checkSession() {
+        //check if user is logged in
+        //if user is logged in --> move to mainActivity
+        SessionManagement sessionManagement = new SessionManagement(getApplicationContext());
+        int userID = sessionManagement.getSession();
+
+        if(userID != -1) {
+            //user id logged in and so move to mainActivity
+            moveToMainActivity();
+        }
+    }
+
+    public void login(View view) {
+        // 1.log in to app and save session of user
+        // 2. move to mainActivity
+
+        //1. login and save session
+        User user = new User(42069,"meteora");
+        SessionManagement sessionManagement = new SessionManagement(getApplicationContext());
+        sessionManagement.saveSession(user);
+
+        //2. step
+        moveToMainActivity();
+    }
+
+    private void moveToMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/vn/edu/usth/minigh/MainActivity.java
+++ app/src/main/java/vn/edu/usth/minigh/MainActivity.java
@@ -2,7 +2,9 @@ package vn.edu.usth.minigh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logout(View view) {
+        SessionManagement sessionManagement = new SessionManagement(getApplicationContext());
+        sessionManagement.removeSession();
+        moveToAuthActivity();
+    }
+
+    private void moveToAuthActivity() {
+        Intent intent = new Intent(getApplicationContext(), AuthActivity.class);
+        startActivity(intent);
     }
 }
