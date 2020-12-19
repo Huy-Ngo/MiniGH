cbae63acf25720c65065840c560fc706f2edd2d5
Phương Trịnh <thaophuong.trinh.9@gmail.com>
2020-12-20 00:20:39 +0700

    Avoid softkeyboard from hidding text field

--- app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
+++ app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
@@ -2,8 +2,11 @@ package vn.edu.usth.minigh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -27,5 +30,15 @@ public class AuthActivity extends AppCompatActivity {
     private void moveToProfileActivity() {
         Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
         startActivity(intent);
+    }
+//    when touch outside hide the keyboard
+//    if view has focus, hiden the keyboard
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
