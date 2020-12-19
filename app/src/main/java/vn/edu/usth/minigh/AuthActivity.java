4ac49d2b38f048452a17e09db38de5068537381f
Ngô Xuân Minh <xminh.ngo.00@gmail.com>
2020-12-19 20:16:56 +0700

    Add splash screen

--- app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
+++ app/src/main/java/vn/edu/usth/minigh/AuthActivity.java
@@ -11,27 +11,9 @@ public class AuthActivity extends AppCompatActivity {
         super(R.layout.activity_auth);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        checkSession();
-    }
-
-    private void checkSession() {
-        //check if user is logged in
-        //if user is logged in --> move to mainActivity
-        SessionManagement sessionManagement = new SessionManagement(getApplicationContext());
-        int userID = sessionManagement.getSession();
-
-        if(userID != -1) {
-            //user id logged in and so move to mainActivity
-            moveToProfileActivity();
-        }
-    }
-
     public void login(View view) {
         // 1.log in to app and save session of user
-        // 2. move to mainActivity
+        // 2. move to profileActivity
 
         //1. login and save session
         User user = new User(42069,"meteora");
---
+++ app/src/main/java/vn/edu/usth/minigh/SplashActivity.java
@@ -0,0 +1,39 @@
+package vn.edu.usth.minigh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final SessionManagement sessionManagement = new SessionManagement(getApplicationContext());
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int userID = sessionManagement.getSession();
+                if(userID != -1) redirectActivity(ProfileActivity.class);
+                else redirectActivity(AuthActivity.class);
+            }
+        }, 2000);
+    }
+
+    public void redirectActivity(Class aclass) {
+        Intent intent = new Intent(this, aclass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
