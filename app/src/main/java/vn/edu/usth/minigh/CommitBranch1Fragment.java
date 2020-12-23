fa1586340ad2f85f04f7654a452d8e1ecdc9b9cd
Ngô Xuân Minh <48543141+9a24f0@users.noreply.github.com>
2020-12-23 14:12:03 +0700

    Fix commit tab

--- app/src/main/java/vn/edu/usth/minigh/CommitBranch1Fragment.java
+++ app/src/main/java/vn/edu/usth/minigh/CommitBranch1Fragment.java
@@ -22,6 +22,6 @@ public class CommitBranch1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_commit_main, container, false);
+        return inflater.inflate(R.layout.fragment_commit_branch1, container, false);
     }
 }
