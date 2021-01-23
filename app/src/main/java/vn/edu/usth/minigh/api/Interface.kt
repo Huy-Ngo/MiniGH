c3f56ef245de6c7f4ae8cf81b6436ab5ac729976
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-01-23 15:54:54 +0700

    Cache Retrofit responses

--- app/src/main/java/vn/edu/usth/minigh/api/Interface.kt
+++ app/src/main/java/vn/edu/usth/minigh/api/Interface.kt
@@ -1,5 +1,10 @@
 package vn.edu.usth.minigh.api
 
+import java.io.File
+
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -13,6 +18,13 @@ interface GitHub {
     suspend fun repos(@Path("username") username: String): Array<MinRepo>
 }
 
+val client = OkHttpClient.Builder()  // FIXME: use context.getCacheDir instead
+    .cache(Cache(File("/data/data/vn.edu.usth.minigh/cache"), 5 * 1024 * 1024))
+    .addInterceptor { chain ->
+        chain.proceed(chain.request().newBuilder().header(
+            "Cache-Control", "public, max-age=3600").build())  // 1 hour
+    }.build()
+
 val github = Retrofit.Builder().baseUrl("https://api.github.com")
-    .addConverterFactory(GsonConverterFactory.create()).build()
+    .addConverterFactory(GsonConverterFactory.create()).client(client).build()
     .create(GitHub::class.java)
