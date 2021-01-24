158395c104c044fe8c8c4f4832951dcf08c7dbba
Ngô Ngọc Đức Huy <duchuy29092000@gmail.com>
2021-01-24 16:08:54 +0700

    Add information to navbar

--- app/src/main/java/vn/edu/usth/minigh/BaseActivity.kt
+++ app/src/main/java/vn/edu/usth/minigh/BaseActivity.kt
@@ -1,17 +1,25 @@
 // Base activity with drawer
 package vn.edu.usth.minigh
 
+import kotlinx.coroutines.launch
+
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.text.Html
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.lifecycleScope
+
+import coil.load
+
+import vn.edu.usth.minigh.api.github
 
 abstract class BaseActivity(layoutID: Int) : AppCompatActivity(layoutID) {
     private lateinit var drawerLayout: DrawerLayout
@@ -20,10 +28,18 @@ abstract class BaseActivity(layoutID: Int) : AppCompatActivity(layoutID) {
         super.onCreate(savedInstanceState)
         drawerLayout = findViewById(R.id.drawer_layout)
 
+        val avatar = findViewById<ImageView>(R.id.avatar)
+        val username = findViewById<TextView>(R.id.nav_username)
         val follower = findViewById<TextView>(R.id.follower_total)
-        follower.setText(Html.fromHtml("<b>"+5+"</b> Follower"))
         val following = findViewById<TextView>(R.id.following_total)
-        following.setText((Html.fromHtml("<b>"+5+"</b> Following")))
+
+        lifecycleScope.launch {
+            val user = github.user("Huy-Ngo") // TODO: Replace this with login
+            username.text = "Huy-Ngo"
+            follower.text = Html.fromHtml("<b>${user.followers}</b> Follower")
+            following.text = (Html.fromHtml("<b>${user.following}</b> Following"))
+            avatar.load(user.avatar_url)
+        }
     }
 
     override fun onPause() {
