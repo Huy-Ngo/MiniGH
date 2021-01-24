1bed17963a251a7522a7cb1e482fdda568fbc5fa
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-01-24 22:19:50 +0700

    Better distinguigh files and folders

--- app/src/main/java/vn/edu/usth/minigh/fragments/TreeFragment.kt
+++ app/src/main/java/vn/edu/usth/minigh/fragments/TreeFragment.kt
@@ -16,22 +16,27 @@ import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.lifecycle.lifecycleScope
 
+import info.androidhive.fontawesome.FontCache
+import info.androidhive.fontawesome.FontTextView
+
 import vn.edu.usth.minigh.R
 import vn.edu.usth.minigh.api.github
 
 class TreeNodeFragment : Fragment(R.layout.fragment_tree_node) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val args = requireArguments()
-        // TODO: handle symlinks and git submodules
-        val type = if (args.getString("type") == "dir") {
-            R.string.fa_folder
-        } else {
-            R.string.fa_file
-        }
-        view.findViewById<TextView>(R.id.content_type).text =
-            getString(type)
         view.findViewById<TextView>(R.id.content_name).text =
             args.getString("name")
+
+        val contentType = view.findViewById<FontTextView>(R.id.content_type)
+        // TODO: handle symlinks and git submodules
+        val type = if (args.getString("type") == "dir") {
+            contentType.text = getString(R.string.fa_folder)
+            contentType.setTypeface(FontCache.get(
+                contentType.context, "fa-solid-900.ttf"))
+        } else {
+            contentType.text = getString(R.string.fa_file)
+        }
     }
 }
 
@@ -40,8 +45,10 @@ class TreeFragment : Fragment(R.layout.fragment_tree) {
         val args = requireArguments()
         lifecycleScope.launch {
             childFragmentManager.commit {
-                github.contents(args.getString("repo name")!!,
-                                args.getString("branch")!!).map {
+                github.contents(
+                    args.getString("repo name")!!,
+                    args.getString("branch")!!
+                ).sortedBy { it.type }.map {
                     add<TreeNodeFragment>(
                         R.id.node_list,
                         args = bundleOf("name" to it.name, "type" to it.type))
