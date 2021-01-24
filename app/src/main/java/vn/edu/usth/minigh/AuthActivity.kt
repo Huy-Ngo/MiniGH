6ac31c1a60818f1e6776b98fdfe7eaa6422e7e46
Ngô Ngọc Đức Huy <duchuy29092000@gmail.com>
2021-01-24 16:03:57 +0700

    Redesign auth activity

--- app/src/main/java/vn/edu/usth/minigh/AuthActivity.kt
+++ app/src/main/java/vn/edu/usth/minigh/AuthActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -15,6 +16,11 @@ class AuthActivity : AppCompatActivity(R.layout.activity_auth) {
 
         startActivity(Intent(getApplicationContext(),
                              ProfileActivity::class.java))
+    }
+
+    fun signUp(view: View) {
+        val browser = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/join"))
+        startActivity(browser)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
